93ce3ddb072b34efe0dfc22944aca14e2faa215d
decagon <decagon@decagons-MacBook-Air.local>
2020-10-01 10:21:42 +0100

    added way to close database connection

--- tests/organization.test.ts
+++ tests/organization.test.ts
@@ -1,7 +1,11 @@
 import supertest from 'supertest';
 const app = require('../src/app');
-const request = supertest(app)
+const request = supertest(app);
+import mongoose from 'mongoose';
 
+afterAll(() => {
+    mongoose.connection.close();
+},20000);
 describe('Organization GET routes', () => {
     //ALL ORGANIZATIONS
     it('should return all organizations on database', async (done) => {
